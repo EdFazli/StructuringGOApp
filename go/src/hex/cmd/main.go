c216ea9b3a4c3dfd8c27b68989a02a811ab0ce46
EdFazli <edfazli92@gmail.com>
2021-12-27 21:14:57 +0800

    updating main.go

--- go/src/hex/cmd/main.go
+++ go/src/hex/cmd/main.go
@@ -4,17 +4,19 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/EdFazli/StructuringGOApp/internal/adapters/core/arithmetic"
 )
 
 func main() {
 	fmt.Println("Main file")
 
-	arithAdapter := arithmetic.NewAdapter()
-	result, err := arithAdapter.Addition(3, 5)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(result)
+	//ports
+	// var core ports.ArithmeticPort
+	// core = arithmetic.NewAdapter()
+
+	// arithAdapter := arithmetic.NewAdapter()
+	// result, err := arithAdapter.Addition(3, 5)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(result)
 }
