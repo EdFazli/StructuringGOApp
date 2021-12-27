8f4a7db6655bb59031fc3e21eeca0b9ff779dc5f
EdFazli <edfazli92@gmail.com>
2021-12-27 20:56:53 +0800

    adding arithmetic.go

--- README.md
+++ README.md
@@ -12,4 +12,6 @@ How to structure your GO application.
 5. `mkdir cmd internal`  
 6. `cd cmd` and create main.go  
 7. `cd ../internal && mkdir ports` and create core.go  
-8. 
+8. `cd ../internal && mkdir adapters`  
+9. `cd adapters && mkdir -p core/arithmetic`  
+10. 
---
+++ go/src/hex/internal/adapters/core/arithmetic/arithmetic.go
@@ -0,0 +1,26 @@
+//adapters for all of our ports
+
+package arithmetic
+
+type Adapter struct {
+}
+
+func NewAdapter() *Adapter {
+	return &Adapter{}
+}
+
+func (arith Adapter) Addition(a int32, b int32) (int32, error) {
+	return a + b, nil
+}
+
+func (arith Adapter) Subtraction(a int32, b int32) (int32, error) {
+	return a - b, nil
+}
+
+func (arith Adapter) Multiplication(a int32, b int32) (int32, error) {
+	return a * b, nil
+}
+
+func (arith Adapter) Division(a int32, b int32) (int32, error) {
+	return a / b, nil
+}
