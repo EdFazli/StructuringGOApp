dbedf5f0fa03c43101863dfe3ae0180c9d1dda0d
EdFazli <edfazli92@gmail.com>
2021-12-28 18:24:23 +0800

    adding db into the api.go adapter

--- go/src/hex/internal/adapters/app/api/api.go
+++ go/src/hex/internal/adapters/app/api/api.go
@@ -7,15 +7,21 @@ import (
 )
 
 type Adapter struct {
+	db    ports.DbPort
 	arith ports.ArithmeticPort
 }
 
-func NewAdapter(arith ports.ArithmeticPort) *Adapter {
-	return &Adapter{arith: arith}
+func NewAdapter(db ports.DbPort, arith ports.ArithmeticPort) *Adapter {
+	return &Adapter{db: db, arith: arith}
 }
 
 func (apiA Adapter) GetAddition(a, b int32) (int32, error) {
 	answer, err := apiA.arith.Addition(a, b)
+	if err != nil {
+		return 0, err
+	}
+
+	err = apiA.db.AddToHistory(answer, "addition")
 	if err != nil {
 		return 0, err
 	}
@@ -29,6 +35,11 @@ func (apiA Adapter) GetSubtraction(a, b int32) (int32, error) {
 		return 0, err
 	}
 
+	err = apiA.db.AddToHistory(answer, "subtraction")
+	if err != nil {
+		return 0, err
+	}
+
 	return answer, nil
 }
 
@@ -38,11 +49,21 @@ func (apiA Adapter) GetMultiplication(a, b int32) (int32, error) {
 		return 0, err
 	}
 
+	err = apiA.db.AddToHistory(answer, "multiplication")
+	if err != nil {
+		return 0, err
+	}
+
 	return answer, nil
 }
 
 func (apiA Adapter) GetDivision(a, b int32) (int32, error) {
 	answer, err := apiA.arith.Division(a, b)
+	if err != nil {
+		return 0, err
+	}
+
+	err = apiA.db.AddToHistory(answer, "division")
 	if err != nil {
 		return 0, err
 	}
