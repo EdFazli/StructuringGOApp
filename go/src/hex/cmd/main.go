3672139c2110a9e57b3e09ec1a12b62231a65d08
EdFazli <edfazli92@gmail.com>
2021-12-29 21:56:46 +0800

    adding dbase adapter in main.go

--- go/src/hex/cmd/main.go
+++ go/src/hex/cmd/main.go
@@ -4,19 +4,38 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/EdFazli/StructuringGOApp/internal/adapters/app/api"
+	"github.com/EdFazli/StructuringGOApp/internal/adapters/core/arithmetic"
+	"github.com/EdFazli/StructuringGOApp/internal/adapters/framework/right/db"
+	"github.com/EdFazli/StructuringGOApp/internal/ports"
+
+	gRPC "github.com/EdFazli/StructuringGOApp/internal/adapters/framework/left/grpc"
 )
 
 func main() {
 	fmt.Println("Main file")
 
-	//ports
-	// var core ports.ArithmeticPort
-	// core = arithmetic.NewAdapter()
+	var err error
 
-	// arithAdapter := arithmetic.NewAdapter()
-	// result, err := arithAdapter.Addition(3, 5)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// fmt.Println(result)
+	//ports
+	var dbaseAdapter ports.DbPort
+	var core ports.ArithmeticPort
+	var appAdapter ports.APIPort
+	var gRPCAdapter ports.GRPCPort
+
+	dbaseDriver := os.Getenv("DB_DRIVER")
+	dsourceName := os.Getenv("DS_NAME")
+	dbaseAdapter, err = db.NewAdapter(dbaseDriver, dsourceName)
+	if err != nil {
+		log.Fatalf("failed to initiate dbase connection: %v", err)
+	}
+	defer dbaseAdapter.CloseDbConnection()
+
+	core = arithmetic.NewAdapter()
+	appAdapter = api.NewAdapter(dbaseAdapter, core)
+	gRPCAdapter = gRPC.NewAdapter(appAdapter)
+	gRPCAdapter.Run()
 }
--- go/src/hex/internal/ports/app.go
+++ go/src/hex/internal/ports/app.go
@@ -3,8 +3,8 @@
 package ports
 
 type APIPort interface {
-	GetAddition(a, b int32) (int32 error)
-	GetSubtraction(a, b int32) (int32 error)
-	GetMultiplication(a, b int32) (int32 error)
-	GetDivision(a, b int32) (int32 error)
+	GetAddition(a, b int32) (int32, error)
+	GetSubtraction(a, b int32) (int32, error)
+	GetMultiplication(a, b int32) (int32, error)
+	GetDivision(a, b int32) (int32, error)
 }
